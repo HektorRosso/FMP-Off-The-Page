f70cb93552aec423eb3ffead23110db40908e801
The Great Bakerloo <iross2024@elam.co.uk>
2026-03-20 10:42:00 +0000

    Enemy behaviour complete

--- Assets/Scripts/Enemies/Ranged.cs
+++ Assets/Scripts/Enemies/Ranged.cs
@@ -44,6 +44,24 @@ public class Ranged : MonoBehaviour
         if (enemyPatrol != null) enemyPatrol.enabled = !PlayerInSight();
     }
 
+    private void RangedAttack()
+    {
+        cooldownTimer = 0;
+        fireballs[FindFireball()].transform.position = firepoint.position;
+        fireballs[FindFireball()].GetComponent<EnemyProjectile>().ActivateProjectile();
+    }
+
+    private int FindFireball()
+    {
+        for (int i = 0; i < fireballs.Length; i++)
+        {
+            if (!fireballs[i].activeInHierarchy)
+                return i;
+        }
+
+        return 0;
+    }
+
     private bool PlayerInSight()
     {
         RaycastHit2D hit =
